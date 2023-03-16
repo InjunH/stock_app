643b0d147ab54ef89287f097111896d1e13e900c
INJUN <ijhwang@pood.pet>
2023-03-16 16:47:34 +0900

    screen 에서 action 정의

--- lib/presentation/company_listings/company_listings_view_model.dart
+++ lib/presentation/company_listings/company_listings_view_model.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:stock_app/domain/repository/stock_repository.dart';
+import 'package:stock_app/presentation/company_listings/company_listings_action.dart';
 import 'package:stock_app/presentation/company_listings/company_listings_state.dart';
 
 class CompanyListingsViewModel with ChangeNotifier {
@@ -8,10 +11,24 @@ class CompanyListingsViewModel with ChangeNotifier {
 
   var _state = CompanyListingsState();
 
+  Timer? _debounce;
+
   CompanyListingsState get state => _state;
 
   CompanyListingsViewModel(this._repository) {
     _getCompanyListings();
+  }
+
+  void onAction(CompanyListingsAction action) {
+    action.when(
+      refresh: () => _getCompanyListings(fetchFromRemote: true),
+      onSearchQueryCahnge: (query) {
+        _debounce?.cancel();
+        _debounce = Timer(const Duration(milliseconds: 500), () {
+          _getCompanyListings(query: query);
+        });
+      },
+    );
   }
 
   Future _getCompanyListings(
