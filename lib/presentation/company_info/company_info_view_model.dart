f3ae517381f4e0c0ec95d9cdb00da239e36f65c3
INJUN <ijhwang@pood.pet>
2023-03-18 13:54:26 +0900

    company info viewModel 추가

---
+++ lib/presentation/company_info/company_info_view_model.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:stock_app/domain/repository/stock_repository.dart';
+import 'package:stock_app/presentation/company_info/company_info_state.dart';
+
+class CompanyInfoViewModel with ChangeNotifier {
+  final StockRepository _repository;
+
+  var _state = const CompanyInfoState();
+
+  CompanyInfoState get state => _state;
+
+  CompanyInfoViewModel(this._repository);
+
+  void loadCompanyInfo(String symbol) async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+
+    final result = await _repository.getCompanyInfo(symbol);
+
+    result!.when(
+      success: (info) {
+        _state = state.copyWith(companyInfo: info, isLoading: false);
+      },
+      error: (e) {
+        _state = state.copyWith(companyInfo: null, isLoading: false);
+      },
+    );
+
+    notifyListeners();
+  }
+}
